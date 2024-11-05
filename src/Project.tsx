b958a57c61d254338cfff7dfd03c55748b907266
Lucas Yepez <lucasyepez99@gmail.com>
2024-11-05 12:17:14 -0600

    added urlfor message app

--- src/Project.tsx
+++ src/Project.tsx
@@ -17,7 +17,7 @@ function Project(){
   const [projects,setProjects] = useState<ProjectType[]>([])
   const [projDetailsId,setProjDetailsId] = useState<string>("");
   useEffect(() => {
-    setProjects([{id:"1",title:"Messaging App",path:"https://github.com/lyepez-glitch/MessagingApp",desc:"Real time Messaging App",demo:"demo",challenges:"integrating websockets",techUsed:"Django Channels, Django",features:"send,receive, update messages in real-time",viewCode:"https://github.com/lyepez-glitch/MessagingApp"},
+    setProjects([{id:"1",title:"Messaging App",path:"https://lyep.pythonanywhere.com/rooms/",desc:"Real time Messaging App",demo:"demo",challenges:"integrating websockets",techUsed:"Django Channels, Django",features:"send,receive, update messages in real-time",viewCode:"https://github.com/lyepez-glitch/MessagingApp"},
     {id:"2",title:"Oracle Ecommerce App",path:"https://github.com/lyepez-glitch/Oracle_Ecommerce_App",desc:"Manage Employees through a management system",demo:"https://lyep.pythonanywhere.com/",challenges:"Multiple CRUD needed",techUsed:"Springboot,Java,React,Oracle PLSQL",features:"add,update,manage employees,roles,departments",viewCode:"https://github.com/lyepez-glitch/Oracle_Ecommerce_App"},
     {id:"3",title:"Collaborative-Task-Management-App",path:"https://github.com/lyepez-glitch/Collaborative-Task-Management-App",desc:"Edit Profile,Manage tasks,projects",demo:"demo",challenges:"using websockets",techUsed:"Node,React",features:"Can add,update,delete tasks and projects, edit profile",viewCode:"https://github.com/lyepez-glitch/Collaborative-Task-Management-App"}])
     },[]);
