a72315af374f7859b44e366d2bf8393c19d8f235
Lucas Yepez <lucasyepez99@gmail.com>
2025-02-04 10:43:38 -0600

    refactored view code

--- src/ProjectDetail.tsx
+++ src/ProjectDetail.tsx
@@ -1,36 +1,52 @@
 import { Link } from 'react-router-dom';
-type ProjDetailProps = {
-  project:{
-    id: string,
-    title: string,
-    desc:string,
-    demo: string;
-    challenges: string,
-    techUsed: string,
-    features: string,
-    viewCode: string
-  }
-}
 
-function ProjectDetail({project}:ProjDetailProps){
+type ProjDetailProps = {
+  project: {
+    id: string;
+    title: string;
+    desc: string;
+    demo: string;
+    challenges: string;
+    techUsed: string;
+    features: string;
+    viewCode: string | string[]; // Can be a string or an array
+  };
+};
+
+function ProjectDetail({ project }: ProjDetailProps) {
   return (
     <>
-    <div className="projDetailsContainer">
-      <div><div style={{display:'block'}}>Challenges:</div>{project.challenges}</div>
-      <br/>
-      <div><div style={{display:'block'}}>Tech used:</div>{project.techUsed}</div>
-      <br/>
-      <div><div style={{display:'block'}}>Features:</div>{project.features}</div>
-      <br/>
-      <Link to={project.demo}>View demo</Link>
-      <br/>
-      <br/>
-      <Link to={project.viewCode}>View Code</Link>
-    </div>
-
+      <div className="projDetailsContainer">
+        <div>
+          <div style={{ display: 'block' }}>Challenges:</div>
+          {project.challenges}
+        </div>
+        <br />
+        <div>
+          <div style={{ display: 'block' }}>Tech used:</div>
+          {project.techUsed}
+        </div>
+        <br />
+        <div>
+          <div style={{ display: 'block' }}>Features:</div>
+          {project.features}
+        </div>
+        <br />
+        <Link to={project.demo}>View demo</Link>
+        <br />
+        <br />
+        {Array.isArray(project.viewCode) ? (
+          project.viewCode.map((repo, index) => (
+            <div key={index}>
+              <Link to={repo}>View Code {index + 1}</Link>
+            </div>
+          ))
+        ) : (
+          <Link to={project.viewCode}>View Code</Link>
+        )}
+      </div>
     </>
-
-  )
+  );
 }
 
 export default ProjectDetail;
